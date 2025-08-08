0a1efc0e49c7f88fb26a68a72be1f8f57b2a9d4d
pavansharan007 <pavansharan1316@gmail.com git config --global.user.name Pavan>
2025-08-08 17:49:52 +0530

    change

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -18,6 +18,22 @@ function MyProjects(post) {
     })
   },[])
   console.log(posts)
+  if(posts.length === 0){
+    return (
+      <div className='flex flex-col items-center justify-center text-white' style={{minHeight:'60vh'}}>
+        <h1 className='text-5xl mb-2'>You Have No Projects Yet</h1>
+        {" "}
+        <p className='mb-5'>Start Creating a new project</p>
+        <button
+            className="bg-black text-white font-semibold px-6 py-3 border border-white rounded-md hover:bg-gray-900 transition"
+            onClick={() => navigate("/add-post")}
+          >
+            Create Project {' > '}
+          </button>
+      </div>
+  
+  )
+  }
   return (
     <div>
       <div className='flex flex-wrap justify-center'>
